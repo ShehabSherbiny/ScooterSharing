2581652e469a19eb3ac2edcd8324ebf44f69eae6
Galal <ahga@itu.dk>
2023-02-27 05:35:19 +0100

    week 2 assignments

--- app/src/main/java/dk/itu/moapd/scootersharing/ahga/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/ahga/MainActivity.kt
@@ -1,42 +1,71 @@
+/*
+MIT License
+
+Copyright (c) [2023] [Ahmed Bassam Galal & Alyson De Souza]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 package dk.itu.moapd.scootersharing.ahga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
 import androidx.core.view.WindowCompat
+import com.google.android.material.snackbar.Snackbar
+import dk.itu.moapd.scootersharing.ahga.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private val TAG = MainActivity :: class.qualifiedName
     }
-    private lateinit var scooterName : EditText
-    private lateinit var location : EditText
-    private lateinit var startRideButton: Button
-    private val scooter : Scooter = Scooter ("", "")
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var scooter : Scooter
+
+    /**
+     * Method for creating a new activity
+     * @param
+     */
     override fun onCreate(savedInstanceState:Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //set variables by getting information from "R"(look into what this is exactly)
-        scooterName = findViewById(R.id.plain_text_input)
-        location = findViewById(R.id.location_input)
-        startRideButton = findViewById(R.id.start_ride_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //set event listener and implement logic
-        startRideButton.setOnClickListener{
-            if(scooterName.text.isNotEmpty() && location.text.isNotEmpty()){
-                val name = scooterName.text.toString().trim()
-                val location2 = location.text.toString().trim()
-                scooter.setName(name)
-                scooter.setLocation(location2)
+        binding.apply {
 
-                //reset textfields and update UI
-                scooterName.text.clear()
-                location.text.clear()
 
-                showMessage()
+            startRideButton.setOnClickListener{
+                if(startRideButton.text.isNotEmpty() && locationInput.text.isNotEmpty()){
+                    val name = nameInput.text.toString().trim()
+                    val location = locationInput.text.toString().trim()
+                    scooter = Scooter(name, location)
+                    val snack = Snackbar.make(it,scooter.toString(),1000)
+                    snack.setAnchorView(startRideButton.id)
+                    snack.show()
+
+                    //reset textfields and update UI
+                    nameInput.text.clear()
+                    locationInput.text.clear()
+
+                }
             }
         }
 
--- app/src/main/java/dk/itu/moapd/scootersharing/ahga/Scooter.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/ahga/Scooter.kt
@@ -1,31 +1,34 @@
+/*
+MIT License
+
+Copyright (c) [2023] [Ahmed Bassam Galal & Alyson De Souza]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 package dk.itu.moapd.scootersharing.ahga
 
-class Scooter {
-    private var name: String
-    private var location: String
+/**
+ * This class is a data class meant to hold information about scooters
+ * @property name is a String; Name of the scooter
+ * @property location is a String; Starting point for the ride
+ */
+data class Scooter(val name: String, val location: String)
 
-    constructor (name: String, location: String) {
-        this.name = name
-        this.location = location
-    }
 
-    fun getName(): String {
-        return name
-    }
 
-    fun setName(name: String) {
-        this.name = name
-    }
-
-    fun getLocation(): String {
-        return location
-    }
-
-    fun setLocation(location: String) {
-        this.location = location
-    }
-
-    override fun toString(): String {
-        return "[ Scooter ] $name is placed at $location."
-    }
-}
