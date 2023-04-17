edb6e4b3cc9db2876db39e5ac0dc213ff38ed32e
Alyson De Souza <alysondsouza@yahoo.com>
2023-04-17 15:57:48 +0200

    Added Bucket

--- app/src/main/java/dk/itu/moapd/scootersharing/ahga/dataClasses/Rides.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/ahga/dataClasses/Rides.kt
@@ -6,7 +6,8 @@ import com.google.firebase.database.IgnoreExtraProperties;
 data class Rides(
     var scooter: String = "unknown",
     val price: Double = 0.0,
-    val startLocation: String = "",
+    val startLocation: String = "start",
+    val endLocation: String = "end",
     val startTime: Long? = null,
     val endTime: Long? = null,
 ) {
